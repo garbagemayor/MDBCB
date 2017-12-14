955f632f68617cf2b8777e38434769ce4068c7ee
nodgd <dcc1234@qq.com>
2017-12-14 19:21:31 +0800

    原有的页式文件系统有些不足之处，这里重新封装一个'单文件管理器'，补充常用类型的typedef，补充几个方便的二进制数据读写函数

---
+++ SourceCode/filesystem/ByteBufBase.h
@@ -0,0 +1,72 @@
+#ifndef BYTE_BUF_BASE_H_
+#define BYTE_BUF_BASE_H_
+
+typedef unsigned char Byte;
+typedef unsigned char * ByteBufType;
+typedef unsigned long long uint64;
+
+///把数据以二进制的方式写入ByteBufType中，并把指针前移
+/*
+ *  @函数名:writeArrayToByte
+ *  @参数buf:写入的地方
+ *  @参数writeLen:写入的长度
+ *  @参数data:需要写入的数据指针
+ *  @参数dataLen:数据有效部分的长度，默认2^31 - 1
+ *  功能:将data的前min(writeLen, dataLen)字节写入buf，如果writeLen > dataLen就用0来填充，并将buf前移writeLen字节
+ */
+void writeArrayToByte(ByteBufType & buf, int writeLen, ByteBufType data, int dataLen = (~0u >> 1)) {
+    for (int i = 0; i < writeLen; i ++) {
+        buf[i] = i < dataLen ? data[i] : 0;
+    }
+    buf += writeLen;
+}
+
+/*
+ *  @函数名:writeNumberToByte
+ *  @参数buf:写入的地方
+ *  @参数length:写入的长度，可以是1,2,4,8
+ *  @参数num:需要写入的数据
+ *  功能:整数num以二进制的形式写入buf
+ *  注意:负数要先转换为相同精度的无符号数再传
+ */
+void writeNumberToByte(ByteBufType & buf, int length, uint64 num) {
+    for (int i = 0; i < length; i ++) {
+        buf[i] = ((ByteBufType) &num) [i];
+    }
+    buf += length;
+}
+
+/*
+ *  @函数名:readByteToArray
+ *  @参数buf:读数据的地方
+ *  @参数readLen:读取的长度
+ *  @参数data:读取后写入的地方
+ *  &参数dataLen:写入位置有效部分的长度，默认2^31 - 1
+ *  功能:从buf读取min(readLen, dataLen)字节写入data，并把buf前移readLen个字节
+ */
+void readByteToArray(ByteBufType & buf, int readLen, ByteBufType data, int dataLen) {
+    for (int i = 0; i < readLen; i ++) {
+        if (i < dataLen) {
+            data[i] = buf[i];
+        }
+    }
+    buf += readLen;
+}
+
+/*
+ *  @函数名:readByteToNumber
+ *  @参数buf:读数据的地方
+ *  @参数length:读取的长度，可以是1,2,4,8
+ *  @返回值:读取的结果
+ *  功能:读取length个Byte，转化为整数类型，并把buf前移length个字节
+ */
+uint64 readByteToNumber(ByteBufType & buf, int length) {
+    uint64 num = 0;
+    for (int i = 0; i < length; i ++) {
+        ((ByteBufType) &num) [i] = buf[i];
+    }
+    buf += length;
+    return num;
+}
+
+#endif // BYTE_BUF_BASE_H_
---
+++ SourceCode/filesystem/OneFileManager.h
@@ -0,0 +1,142 @@
+#ifndef ONE_FILE_MANAGER_H_
+#define ONE_FILE_MANAGER_H_
+
+#include "fileio/FileManager.h"
+#include "bufmanager/BufPageManager.h"
+#include "ByteBufBase.h"
+
+#include <string>
+#include <map>
+
+/**
+ *  单文件管理器。
+ *  原有的文件管理器和缓存页管理器都没有记录某个文件内一共有多少页，以及一些其他信息，因此需要封装。
+ *  调用这个类的时候，对页的操作都使用页编号。
+ *  每次调用bufPageManager的hash去获取页在缓存管理器里面的编号非常慢，于是在这个类里面稍微优化了一下。
+ */
+class OneFileManager {
+    
+public:
+    //文件名
+    std::string fileName;
+    //文件编号
+    int fileId;
+    //页数统计
+    int pageCnt;
+    //缓存页管理器
+    BufPageManager * bufPageManager;
+    
+protected:
+    //页面在缓存页管理器里面的编号，要与hash里面的同步添加和删除
+    std::map<int, int> indexMap;
+    
+public:
+    //构造函数，默认是打开已存在的文件
+    OneFileManager(BufPageManager * bufPageManager_, std::string fileName_, bool isNewFile = false) {
+        fileName = fileName_;
+        bufPageManager = bufPageManager_;
+        if (isNewFile) {
+            bufPageManager -> fileManager -> createFile(fileName.c_str());
+            pageCnt = 0;
+        } else {
+            pageCnt = bufPageManager -> fileManager -> getFileSize(fileName.c_str()) / PAGE_SIZE;
+        }
+        if (bufPageManager -> fileManager -> openFile(fileName.c_str(), fileId) == false) {
+            std::cout << "OneFileManager() error" << std::endl;
+            return;
+        }
+        std::cout << "OneFileManager() fildId = " << fileId << std::endl;
+    }
+    
+protected:
+    //由页编号获取缓存页管理器中的编号，并可以顺便删除
+    int getPageIndex(int pageId, bool eraseFlag) {
+        int index = -1;
+        if (indexMap.find(pageId) == indexMap.end()) {
+            index = bufPageManager -> hash -> findIndex(fileId, pageId);
+        } else {
+            index = indexMap[pageId];
+            if (eraseFlag) {
+                indexMap.erase(pageId);
+            }
+        }
+        return index;
+    }
+    
+public:
+    ///对文件内的页进行操作
+    //由页编号获取页数据
+    ByteBufType getPage(int pageId) {
+        int index;
+        BufType data = bufPageManager -> getPage(fileId, pageId, index);
+        indexMap[pageId] = index;
+        return (ByteBufType) data;
+    }
+    
+    //创建一个新的页，获取页编号和页数据
+    ByteBufType getNewPage(int & pageId) {
+        pageId = pageCnt;
+        pageCnt ++;
+        int index;
+        BufType data = bufPageManager -> getPage(fileId, pageId, index);
+        indexMap[pageId] = index;
+        return (ByteBufType) data;
+    }
+    
+    //创建一个新的页，不关心页编号，获取页数据
+    ByteBufType getNewPage() {
+        int pageId;
+        return getNewPage(pageId);
+    }
+    
+    //由页编号标记一个页被访问过，让替换算法尽量不要替换它
+    void access(int pageId) {
+        int index = getPageIndex(pageId, false);
+        if (index == -1) {
+            std::cout << "OneFileManager.access() error" << std::endl;
+            return;
+        }
+        bufPageManager -> access(index);
+    }
+    
+    //由页编号标记脏页
+    void markDirty(int pageId) {
+        int index = getPageIndex(pageId, false);
+        if (index == -1) {
+            std::cout << "OneFileManager.markDirty() error" << std::endl;
+            return;
+        }
+        bufPageManager -> markDirty(index);
+    }
+    
+    //由页编号释放一个页面，这个页面必须是干净的
+    void release(int pageId) {
+        int index = getPageIndex(pageId, true);
+        if (index == -1) {
+            std::cout << "OneFileManagerwriteBack.release() error" << std::endl;
+            return;
+        }
+        bufPageManager -> release(index);
+    }
+    
+    //由页编号来写回一个修改完成的页面，自动选择是直接释放还是写回再释放
+    void writeBack(int pageId) {
+        int index = getPageIndex(pageId, true);
+        if (index == -1) {
+            std::cout << "OneFileManager.writeBack() error" << std::endl;
+            return;
+        }
+        bufPageManager -> writeBack(index);
+    }
+    
+    //关闭这个文件，把打开的所有页都写回
+    void close() {
+        for (std::map<int, int>::iterator ite = indexMap.begin(); ite != indexMap.end(); ite ++) {
+            bufPageManager -> writeBack(ite -> first);
+        }
+        indexMap.clear();
+    }
+};
+
+
+#endif // ONE_FILE_MANAGER_H_
